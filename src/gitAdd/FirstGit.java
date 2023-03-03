cd2fe661c460343c6aa8df1c9e76e60877dbd3a9
Vishal Rohidas Kate <vishalrk@maveric-systems.com>
2023-03-03 17:49:04 +0530

    Added test user name

--- src/gitAdd/FirstGit.java
+++ src/gitAdd/FirstGit.java
@@ -4,6 +4,11 @@ public class FirstGit {
 
 	public static void main(String[] args) {
 		System.out.println("My first branch");
+		
+		String firstName = "Maveric";
+		String lastName = "User";
+		
+		System.out.println(firstName + " " +lastName);
 
 	}
 
