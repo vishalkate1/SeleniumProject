4af21da91bb38f580f55470558e8b0762f04f625
Vishal Rohidas Kate <vishalrk@maveric-systems.com>
2023-03-03 16:31:27 +0530

    second commit

---
+++ src/gitAdd/FirstGit.java
@@ -0,0 +1,13 @@
+package gitAdd;
+
+public class FirstGit {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		System.out.println("Welcome in Git");
+		System.out.println(a+b);	
+
+	}
+
+}
