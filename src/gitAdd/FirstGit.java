202e104d2dced48c237ab21e3534cd057732ea40
Vishal Rohidas Kate <vishalrk@maveric-systems.com>
2023-03-03 17:50:18 +0530

    Changed first name and last name

--- src/gitAdd/FirstGit.java
+++ src/gitAdd/FirstGit.java
@@ -5,8 +5,8 @@ public class FirstGit {
 	public static void main(String[] args) {
 		System.out.println("My first branch");
 		
-		String firstName = "Maveric";
-		String lastName = "User";
+		String firstName = "Vishal";
+		String lastName = "Kate";
 		
 		System.out.println(firstName + " " +lastName);
 
