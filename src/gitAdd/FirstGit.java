25a1cbb7ec9fdeba3d5a63e0c584b5edbc035682
Vishal Rohidas Kate <vishalrk@maveric-systems.com>
2023-03-03 17:31:09 +0530

    My first branch

--- src/gitAdd/FirstGit.java
+++ src/gitAdd/FirstGit.java
@@ -3,10 +3,7 @@ package gitAdd;
 public class FirstGit {
 
 	public static void main(String[] args) {
-		int a=20;
-		int b=30;
-		System.out.println("Welcome in Git");
-		System.out.println(a+b);	
+		System.out.println("My first branch");
 
 	}
 
---
+++ test/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vishalrk
+ *
+ */
+module test {
+}
